d29874befc99b36f3333c7a70be1ff0c2627898d
Le Minh Nhat <leminhnhat1133@gmail.com>
2020-11-15 13:35:48 +0700

    Khoi Tao

---
+++ CinemaBookingTicket/WebAppCinemaBooking/Areas/Admin/Data/DPContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppCinemaBooking.Areas.Admin.Models;
+
+namespace WebAppCinemaBooking.Areas.Admin_0306181057.Data
+{
+    public class DPContext : DbContext
+    {
+        public DPContext(DbContextOptions<DPContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ CinemaBookingTicket/WebAppCinemaBooking/Areas/Admin/Models/Cinema.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCinemaBooking.Areas.Admin.Models
+{
+    public class Cinema
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Tên không được bỏ trống")] public string Name { get; set; }
+        [Required(ErrorMessage = "Địa chỉ không được bỏ trống")] public string Address { get; set; }
+        [Required(ErrorMessage = "Số Di không được bỏ trống")] public string Hotline { get; set; }
+        [Required(ErrorMessage = "Quản lí không được bỏ trống")] public int Manager_ID { get; set; }
+        [ForeignKey("Manager_ID")]
+        [Required(ErrorMessage = "Trạng thái không được bỏ trống")] public int Status { get; set; }
+        public virtual Staf Staf { get; set; }
+    }
+}
