83ca3d4e63327410709368bb9b010ae226724c8d
Huynh <72245217+nmhyh@users.noreply.github.com>
2020-11-19 10:39:48 +0700

    customer

---
+++ CinemaBookingTicket/WebAppCinemaBooking/Areas/Admin/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCinemaBooking.Areas.Admin.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Tên không được bỏ trống")] public string Name { get; set; }
+        [Required(ErrorMessage = "Password không được bỏ trống")] public string Pass { get; set; }
+        [Required(ErrorMessage = "Email không được bỏ trống")] public string Email { get; set; }
+        public string BirthDay { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public decimal Total_Spending { get; set; }
+        public string Avatar { get; set; }
+        [Required(ErrorMessage = "Trạng thái không được bỏ trống")] public int Status { get; set; }
+    }
+}
