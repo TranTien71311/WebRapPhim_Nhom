556a14910b8b3b4b328f187e2aa0005471950024
TranTien71311 <trantien5.8.2000@gmail.com>
2020-11-30 14:49:13 +0700

    Ajax Status Movie, Cinema

---
+++ CinemaBookingTicket/WebAppCinemaBooking/Areas/API/Controllers/CinemasAPIController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebAppCinemaBooking.Areas.Admin.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAppCinemaBooking.Areas.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CinemasAPIController : ControllerBase
+    {
+        private readonly DPContext _context;
+
+        public CinemasAPIController(DPContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public string ChangeStatus(int id)
+        {
+            var cinema = _context.Cinemas.FirstOrDefault(m => m.ID == id);
+            if (cinema.Status == 1)
+                cinema.Status = 0;
+            else
+                cinema.Status = 1;
+            _context.Cinemas.Update(cinema);
+            _context.SaveChanges();
+            return JsonConvert.SerializeObject(cinema);
+        }
+
+    }
+}
---
+++ CinemaBookingTicket/WebAppCinemaBooking/Areas/API/Controllers/MoviesAPIController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebAppCinemaBooking.Areas.Admin.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAppCinemaBooking.Areas.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MoviesAPIController : ControllerBase
+    {
+        private readonly DPContext _context;
+
+        public MoviesAPIController(DPContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public string ChangeStatus(int id)
+        {
+            var movie = _context.Movies.FirstOrDefault(m => m.ID == id);
+            if (movie.Status == 1)
+                movie.Status = 0;
+            else
+                movie.Status = 1;
+            _context.Movies.Update(movie);
+            _context.SaveChanges();
+            return JsonConvert.SerializeObject(movie);
+        }
+    }
+}
